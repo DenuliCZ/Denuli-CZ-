333e2c11b4459b5ed8c8636f174d3abecdc6ee7b
Denisa <denulinkacervinkova@gmail.com>
2026-06-05 15:00:45 +0200

    Delete build.gradle.kts
    Signed-off-by: Denisa  <denulinkacervinkova@gmail.com>

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.secrets) apply false
-}
