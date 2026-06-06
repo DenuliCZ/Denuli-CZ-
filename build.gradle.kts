b7b2a4897cd7fc002415b832d054b8ea9a39acf2
Denisa <denulinkacervinkova@gmail.com>
2026-06-06 18:22:55 +0200

    build: update Android debug APK and Gradle caches

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.secrets) apply false
+}
