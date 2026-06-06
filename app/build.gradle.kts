080c8f9f178db7ac8319261840cdab331c927dac
Denisa <denulinkacervinkova@gmail.com>
2026-06-06 19:33:10 +0200

    build: update debug build artifacts
    Rebuild application debug APK and update local Gradle execution caches.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.studiodenuli.spark"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "34.0"
+        versionCode = 36
+        versionName = "36.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
